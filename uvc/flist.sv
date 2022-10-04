33f62a25284302ca442830a5ef5e9cc525c7d0fd
usman1515 <usmansiddique1515@gmail.com>
2022-10-04 14:44:07 +0500

    created uvm tb flist

---
+++ uvc/flist.sv
@@ -0,0 +1,12 @@
+`timescale 1ns/100ps
+
+// -------------------------------- pre defined macros
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+// -------------------------------- UVM Environment YAPP Packet
+`include "./yapp/yapp_packet.sv"
+
+// --------------------------------------------------------- Sequences
+
+// --------------------------------------------------------- Test cases
