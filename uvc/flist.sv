33e08cb1200223a4f4e4efdac3f3c19b1708b655
usman1515 <usmansiddique1515@gmail.com>
2022-10-05 18:56:28 +0500

    updated flist

--- uvc/flist.sv
+++ uvc/flist.sv
@@ -4,9 +4,17 @@
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 
+// -------------------------------- UVM Environment Channel
+// -------------------------------- UVM Environment Clock and Reset
+// -------------------------------- UVM Environment HBUS
 // -------------------------------- UVM Environment YAPP Packet
 `include "./yapp/yapp_packet.sv"
+
+// -------------------------------- UVM tb
+`include "./tb/router_tb.sv"
 
 // --------------------------------------------------------- Sequences
 
 // --------------------------------------------------------- Test cases
+`include "../tests/router/router_base_test.sv"
+`include "../tests/router/test2.sv"
