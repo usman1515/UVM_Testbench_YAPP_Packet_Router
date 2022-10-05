cb32148122a46c1d29711460d6102f9451b99ed0
usman1515 <usmansiddique1515@gmail.com>
2022-10-05 18:53:55 +0500

    updated tb top

--- uvc/tb/tb_top.sv
+++ uvc/tb/tb_top.sv
@@ -1,29 +1,8 @@
-// `include "flist.sv"
-// `include "../../yapp/tb/flist.sv"
-// `include "./uvc/yapp/tb/flist.sv"
-// -----------------------------------------
-// uvc/flist.sv
-// `include "../../uvc/flist.sv"
-
 module tb_top;
 
-    yapp_packet pkt1;
-    yapp_packet pkt2;
-    yapp_packet pkt3;
-
     initial begin
-        for (int i=0; i<10; i=i+1) begin
-            pkt1 = new("pkt1");
-            assert(pkt1.randomize());
-            $display("Generating packet: %d", i);
-            // pkt1.print(uvm_default_tree_printer);
-            pkt1.print(uvm_default_table_printer);
-            // pkt1.print(uvm_default_line_printer);
-            $display("\n\n");
-        end
+        run_test();
     end
-
-
 
     // ------------------------------------------ enable the wave dump
     `ifdef VCD
@@ -46,6 +25,5 @@ module tb_top;
         $vcdpluson(0, tb_top);
     end
     `endif
-
 
 endmodule : tb_top
