dee8b3ced6882e62a532fea3c204390ace4b526b
usman1515 <usmansiddique1515@gmail.com>
2022-10-10 18:51:18 +0500

    updated flist

--- uvc/flist.sv
+++ uvc/flist.sv
@@ -21,9 +21,19 @@ import uvm_pkg::*;
 
 // --------------------------------------------------------- Sequences
 `include "../sequences/yapp/yapp_base_seq.sv"
-`include "../sequences/yapp/yapp_5_packets_seq.sv"
+`include "../sequences/yapp/yapp_5_pkt_seq.sv"
+`include "../sequences/yapp/yapp_addr_1_seq.sv"
+`include "../sequences/yapp/yapp_addr_012_seq.sv"
+`include "../sequences/yapp/yapp_3_pkt_addr_1_seq.sv"
+`include "../sequences/yapp/yapp_2_pkt_repeat_addr_seq.sv"
+`include "../sequences/yapp/yapp_incr_payload_seq.sv"
+`include "../sequences/yapp/yapp_random_count_pkt_seq.sv"
+`include "../sequences/yapp/yapp_6_pkt_seq.sv"
+`include "../sequences/yapp/yapp_exhaustive_seq.sv"
 
 // --------------------------------------------------------- Test cases
 `include "../tests/router/router_base_test.sv"
 `include "../tests/router/router_short_packet_test.sv"
 `include "../tests/router/router_set_config_test.sv"
+`include "../tests/router/router_incr_payload_test.sv"
+`include "../tests/router/router_exhaustive_test.sv"
